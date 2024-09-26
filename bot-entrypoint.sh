e2c3386b26533c27cfe13053598e19c86fd7e6f4
wanna <wanna@fakemail.com>
2024-09-27 00:15:11 +0300

    Это полный пиздец!!!!

--- bot-entrypoint.sh
+++ bot-entrypoint.sh
@@ -5,4 +5,4 @@ git clone https://github.com/Bahr23/burin_music_bot.git /app/src || true
 cd /app/src
 git pull --force
 mv /app/config.py /app/src/app/.
-python3 -u /app/src/app/app/main.py
+python3 -u /app/src/app/main.py
